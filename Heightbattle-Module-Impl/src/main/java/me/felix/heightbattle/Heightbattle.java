17e0454110d7b751f21cb038a72b37cbf4a4add6
Felix B <f.behme@tu-bs.de>
2024-02-11 14:46:48 +0100

    heightbattle main

---
+++ Heightbattle-Module-Impl/src/main/java/me/felix/heightbattle/Heightbattle.java
@@ -0,0 +1,22 @@
+package me.felix.heightbattle;
+
+import me.felix.gamemodule.GameModuleBootstrap;
+import me.felix.gamemodule.module.Module;
+import me.felix.heightbattle.listener.PlayerJoinListener;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import org.bukkit.Bukkit;
+import org.bukkit.event.Listener;
+
+public class Heightbattle extends Module {
+
+    @Override
+    public void enableModule() {
+        Bukkit.broadcast(
+                MiniMessage.miniMessage().deserialize(GameModuleBootstrap.getInstance().getPrefix() + "Module wurde geladen.")
+        );
+
+        System.out.println("Jojo");
+
+        registerListener(new PlayerJoinListener());
+    }
+}
